7a3242dac94ea39d336e1fa1a05fb714f21c9f8a
nanyatti <34296375+nanyatti@users.noreply.github.com>
2018-02-16 22:16:05 +0900

    UIとプログラムの基盤

--- socket-uwp-csharp/MainPage.xaml.cs
+++ socket-uwp-csharp/MainPage.xaml.cs
@@ -13,6 +13,8 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
+using Windows.Networking.Sockets;
+
 // 空白ページの項目テンプレートについては、https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x411 を参照してください
 
 namespace socket_uwp_csharp
@@ -25,6 +27,41 @@ namespace socket_uwp_csharp
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                //Create the StreamSocket and establish a connection to the echo server.
+                Windows.Networking.Sockets.StreamSocket socket = new Windows.Networking.Sockets.StreamSocket();
+
+                //The server hostname that we will be establishing a connection to. We will be running the server and client locally,
+                //so we will use localhost as the hostname.
+                Windows.Networking.HostName serverHost = new Windows.Networking.HostName("133.37.56.223");
+
+                //Every protocol typically has a standard port number. For example HTTP is typically 80, FTP is 20 and 21, etc.
+                //For the echo server/client application we will use a random port 1337.
+                string serverPort = "50005";
+                await socket.ConnectAsync(serverHost, serverPort);
+
+                //Write data to the echo server.
+                //Stream streamOut = socket.OutputStream.AsStreamForWrite();
+                //StreamWriter writer = new StreamWriter(streamOut);
+                //string request = textbox.Text;
+                //await writer.WriteLineAsync(request);
+                //await writer.FlushAsync();
+
+                //Read data from the echo server.
+                Stream streamIn = socket.InputStream.AsStreamForRead();
+                StreamReader reader = new StreamReader(streamIn);
+                string response = await reader.ReadLineAsync();
+                textblock.Text = response;
+            }
+            catch (Exception)
+            {
+                //Handle exception here.            
+            }
         }
     }
 }
